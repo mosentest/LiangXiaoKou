1bb8522ac0c754f286fd54ed5cd62cf57d2d7c18
moziqi <709847739@qq.com>
2016-05-19 18:08:10 +0800

    实现服务上传信息

---
+++ app/src/main/java/org/liangxiaokou/module/album/IAlbumView.java
@@ -0,0 +1,10 @@
+package org.liangxiaokou.module.album;
+
+import org.liangxiaokou.app.IView;
+
+/**
+ * Created by Administrator on 2016/5/19.
+ */
+public interface IAlbumView extends IView {
+
+}
